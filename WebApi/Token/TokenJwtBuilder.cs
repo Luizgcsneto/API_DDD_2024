c385a2b4fe888e1bea4cc02049b9e1c846b74881
Luiz Gonzaga <luizgonzaga15@gmail.com>
2024-01-10 18:21:23 -0300

    Finalização por enquanto da classe de TokenJwrBuilder

--- WebApi/Token/TokenJwtBuilder.cs
+++ WebApi/Token/TokenJwtBuilder.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace WebApi.Token
 {
@@ -45,6 +47,45 @@ namespace WebApi.Token
         {
             this.expireInMinutes = expireInMinutes;
             return this;
+        }
+
+        private void EnsureArguments()
+        {
+            if (this.securityKey == null)
+                throw new ArgumentNullException("Security Key");
+
+            if (string.IsNullOrEmpty(this.subject))
+                throw new ArgumentNullException("Subject");
+
+            if (string.IsNullOrEmpty(this.issuer))
+                throw new ArgumentNullException("Issuer");
+
+            if (string.IsNullOrEmpty(this.audience))
+                throw new ArgumentNullException("Audience");
+        }
+
+        public TokenJwt Builder()
+        {
+            EnsureArguments();
+
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,this.subject),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            }.Union(this.claims.Select(item => new Claim(item.Key, item.Value)));
+
+            var token = new JwtSecurityToken(
+                issuer: this.issuer,
+                audience: this.audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(expireInMinutes),
+                signingCredentials: new SigningCredentials(
+                    this.securityKey,
+                    SecurityAlgorithms.HmacSha256)
+                );
+
+            return new TokenJwt(token);
+
         }
     }
 }
