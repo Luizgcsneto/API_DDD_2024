0d9418bc53949644770925a54891253876e7d486
Luiz Gonzaga <luizgonzaga15@gmail.com>
2024-01-10 12:49:56 -0300

    Criação da classe Notificações

---
+++ Entities/Entities/Notifies.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Entities
+{
+    public class Notifies
+    {
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+        [NotMapped]
+        public string Mensagem { get; set; }
+        [NotMapped]
+        public List<Notifies> Notificacoes { get; set; }
+    }
+}
