52d4cc53193de8ccc807d94d26402f278927a3fc
Luiz Gonzaga <luizgonzaga15@gmail.com>
2024-01-11 11:17:42 -0300

    Criação do controller de mensagem e criação do retorno para buscar o id do usuário

---
+++ WebApi/Controllers/MessagesController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Domain.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly IMapper _Imapper;
+        private readonly IMessage _Imessage;
+        public MessagesController(IMapper Imapper, IMessage Imessage)
+        {
+            _Imapper = Imapper;
+            _Imessage = Imessage;
+        }
+
+        [Authorize]
+        [Produces("application/json")]
+        [HttpPost("/api/CriarTokenIdentity")]
+        public async Task<List<Notifies>> Add(MessageViewModel message)
+        {
+
+        }
+
+
+        private async Task<string> RetornarIdUsuarioLogado()
+        {
+            if (User != null)
+            {
+                var idUsuario = User.FindFirst("idUsuario");
+                return idUsuario.Value;
+            }
+
+            return string.Empty;
+        }
+    }
+}
